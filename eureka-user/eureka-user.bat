17f200bbc87e95a23b178122312cfdd046e9aa45
ck <2464017733@qq.com>
2019-01-22 22:18:21 +0800

    1

--- eureka-user/eureka-user.bat
+++ eureka-user/eureka-user.bat
@@ -1,4 +1,4 @@
-cd C:\company\product\paifang\server\api\eureka-server
+cd C:\company\product\paifang\server\api\microservice-user\eureka-user
 call mvn docker:removeImage
 call mvn clean package  docker:build -s C:\company\product\apache-maven-3.5.3\conf\settings.xml
 mvn clean
